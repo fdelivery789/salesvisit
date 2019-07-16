e1051e7953d01b404f4100e194913f0975de9445
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-07-16 17:49:12 +0700

    commit

--- db.php
+++ db.php
@@ -1,3 +1,3 @@
 <?php
-$c = new mysqli("localhost", "u117143075_food", "u117143075_food");
-$c->select_db("salesvisit");
+$c = new mysqli("localhost", "u117143075_sales", "Hello123");
+$c->select_db("u117143075_sales");
