fda55f0f80eaa44acf25ae3f5dfb4daae04dd071
Dana Prakoso Eka Saputra <danaoscompany@gmail.com>
2019-07-16 18:04:43 +0700

    commit

--- mail-test.php
+++ mail-test.php
@@ -1,2 +1,9 @@
 <?php
-mail("danaoscompany@gmail.com", "Atur ulang kata sandi", "This is message");
+$to      = 'danaoscompany@gmail.com';
+$subject = 'Atur ulang kata sandi';
+$message = 'This is message 2';
+$headers = 'From: fdelivery789@gmail.com' . "\r\n" .
+    'Reply-To: danaoscompany@gmail.com' . "\r\n" .
+    'X-Mailer: PHP/' . phpversion();
+mail($to, $subject, $message, $headers);
+?> 
